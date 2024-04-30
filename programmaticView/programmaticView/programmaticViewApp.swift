14229373be052bddc762d9bdaeeaa25a6e735133
SkyPW <161071809+SkyPW@users.noreply.github.com>
2024-04-30 12:47:23 -0500

    Add files via upload

---
+++ programmaticView/programmaticView/programmaticViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  programmaticViewApp.swift
+//  programmaticView
+//
+//  Created by  on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct programmaticViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
